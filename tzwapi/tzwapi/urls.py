297647c50d91c27f26a6d7928fd5747ae593a735
Zenen039 <2151050020anh@ou.edu.vn>
2025-02-27 21:32:10 +0700

    update

--- tzwapi/tzwapi/urls.py
+++ tzwapi/tzwapi/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from tzwapp.admin import admin_site
 from django.urls import path, include, re_path
 
 urlpatterns = [
     path('', include('tzwapp.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin_site.urls),
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls'))
 ]
--- tzwapi/tzwapp/admin.py
+++ tzwapi/tzwapp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.db.models import Count
+from django.template.response import TemplateResponse
 from django.utils.safestring import mark_safe
-
 from tzwapp.models import Category, Course, Lesson, Tag
 from django import forms
+from django.urls import path
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 
@@ -21,11 +23,29 @@ class MyLessonAdmin(admin.ModelAdmin):
     readonly_fields = ['image_view']
     form = LessonForm
 
-    def image_view(self, lesson):
+    @staticmethod
+    def image_view(lesson):
         return mark_safe(f"<img src='/static/{lesson.image.name}' width='200' />")
 
 
-admin.site.register(Category)
-admin.site.register(Course)
-admin.site.register(Lesson, MyLessonAdmin)
-admin.site.register(Tag)
+class MyAdminSite(admin.AdminSite):
+    site_header = 'OU Online Course'
+
+    def get_urls(self):
+        return [path('course_stats/', self.course_stats)] + super().get_urls()
+
+    def course_stats(self, request):
+        stats = Category.objects.annotate(course_count=Count('course__id')).values('id', 'name', 'course_count')
+
+        return TemplateResponse(request, 'admin/course_stats.html', {
+            'stats': stats
+        })
+
+
+admin_site = MyAdminSite(name='TZW_Course')
+
+
+admin_site.register(Category)
+admin_site.register(Course)
+admin_site.register(Lesson, MyLessonAdmin)
+admin_site.register(Tag)
